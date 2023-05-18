d091e84e0e85e6fc474374706b219d34abf666b9
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-18 12:12:25 +0800

    fixed. as well {project.parent.version} can be used in module.

--- jpa-entities/src/main/java/guru/springframework/entities/User.java
+++ jpa-entities/src/main/java/guru/springframework/entities/User.java
@@ -5,8 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 /**
  * Created by jt on 2018-12-15.
